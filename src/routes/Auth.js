4de27bef0a7f9870505dac882105006ba3ec3650
Keunrim Baek <neorimy@gmail.com>
2022-10-05 18:57:09 +0900

    Auth Form
    * Auth Form 제작중...

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    //이 방식으로하면 동일한 두개의 fn을 만들지 않아도 된다.
+    console.log(event.target.name);
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
+    } else {
+      setPassword(event.target.value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        ></input>
+        <input type="submit" value="Log in" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
