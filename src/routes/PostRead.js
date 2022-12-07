6c8c76184bfb60128a2a9501c61cd797f6141c7b
Keunrim Baek <neorimy@gmail.com>
2022-12-07 16:14:31 +0900

    Update PostRead.js

--- src/routes/PostRead.js
+++ src/routes/PostRead.js
@@ -10,10 +10,14 @@ import {
   getCountFromServer,
   endBefore,
   limitToLast,
+  Timestamp,
 } from "firebase/firestore";
 
 //antd
-import { Pagination } from "antd";
+import { DatePicker } from "antd";
+import "dayjs/locale/ko";
+import locale from "antd/es/date-picker/locale/en_US";
+const { RangePicker } = DatePicker;
 
 const PostRead = () => {
   const collectionRef = collection(dbService, "posts");
@@ -42,7 +46,6 @@ const PostRead = () => {
       setStartPost(startPostSnap);
       const endPostSnap = dataSnap.docs[dataSnap.docs.length - 1];
       setEndPost(endPostSnap);
-      console.log(endPostSnap);
     };
 
     //전체 포스팅 수 집계
@@ -67,6 +70,7 @@ const PostRead = () => {
   return (
     <>
       <h1>PostRead</h1>
+      <RangePicker locale={locale} />
       <div>
         {posts.map((post) => (
           <div key={post.id} style={{ marginTop: "20px" }}>
@@ -74,13 +78,6 @@ const PostRead = () => {
           </div>
         ))}
       </div>
-      <Pagination
-        size="small"
-        current={currentPage}
-        pageSize={pageSize}
-        total={totalPosts}
-        onChange={handleChange}
-      />
     </>
   );
 };
